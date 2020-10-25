7b55d6b474c549ba813477bb2f55b422316b5bdf
Freniel Zabala <frenielzabala@freniels-mbp.lan>
2020-10-25 15:41:10 -0700

    added sideways triangle problem:

---
+++ pure_puzzles/sideways_triangle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+void createSidewaysTriangle(){
+    int poundCount=1;
+    int switcher=1;
+    for(int i=0;i < 7;i++){
+        for(int j=0;j < poundCount ; j++){
+            std::cout << '#';
+        }
+        if(i == 3) {
+            switcher= -1;
+        }
+        poundCount = poundCount + (switcher * 1);
+        std::cout << '\n';
+    }
+}
+
+int main() {
+    createSidewaysTriangle();
+}
