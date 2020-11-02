97269f1c6f31858219e751ea1f18063151abf871
Freniel Zabala <frenielzabala@freniels-mbp.lan>
2020-11-01 18:37:40 -0800

    added helper script and class for c program

--- pure_puzzles/luhn_checksum_validation.cpp
+++ pure_puzzles/luhn_checksum_validation.cpp
@@ -1,33 +1,18 @@
 #include <iostream>
-#include <typeinfo>
 
-//<TODO: do this with cin.get()>> and convert character to number
-int main() {
-    int total=0;
-    int round=1;
-    int number;
-    while(std::cin >> number){
-        if(round%2==0) {
-            number=number*2;
-            if(number > 10){
-                total+=1;
-                total+=number%10;
-            }
-            else {
-                total+=number;
-            }
-        }
-        else {
-            total+=number;
-        }
+class LuhnCheckSumValidation {
+  public:
+    void simulation();
+};
 
-        round++;
-    } 
-    
-    if(total%10 == 0){
-        std::cout << total << " valid number" << std::endl;
+void LuhnCheckSumValidation::simulation(){
+    char number;
+    while(std::cin.get(number)){
+        std::cout << number;
     }
-    else {
-        std::cout << total << "invalid number" << std::endl;
-    }
+}
+
+int main(){
+    LuhnCheckSumValidation lcsv;
+    lcsv.simulation();
 }
