45d8af474477fd9c57a8bd4817dccfb6af27f3ca
Freniel Zabala <frenielzabala@freniels-mbp.lan>
2020-11-12 22:03:03 -0800

    finished decoder

--- pure_puzzles/decoder.cpp
+++ pure_puzzles/decoder.cpp
@@ -1,26 +1,17 @@
 #include <iostream>
 
-/*
- * Don't need map since ascii key is already a map from int to char
- * Diff between capital and lowercase is 32
- * Diff 1 - 65, diff between 1-27 and 65-90 is 64
- * (x * 10) + newNum
- * Use switch statements for punctuation or if statement
- */
-
 class Decoder {
     private:
-        int state,  UPPERCASE=0, LOWERCASE=1, PUNCTUATION=2;
+        int state; 
+        static const int UPPERCASE=0, LOWERCASE=1, PUNCTUATION=2;
         void switchState();
         char uppercaseDecode(int num);
         char lowercaseDecode(int num);
         char punctuationDecode(int num);
         int modCase(int num);
         int modPunctuation(int num);
-        int modNumber(int num);
         int charToInt(char digit);
-        char decode(int num);
-        void decodeLetter(int num);
+        void decode(int num);
     public:
         Decoder();
         void decodeMessage();
@@ -35,7 +26,82 @@ void Decoder::switchState() {
         state=PUNCTUATION;
     }
     else if(state == PUNCTUATION) {
-        state==UPPERCASE;
+        state=UPPERCASE;
+    }
+}
+
+char Decoder::uppercaseDecode(int num){
+    return (char) (num + 64);
+}
+
+char Decoder::lowercaseDecode(int num){
+    return (char) (num + 96);
+}
+
+char Decoder::punctuationDecode(int num){
+    if(num == 1) {
+        return '!';
+    }
+    else if(num == 2) {
+        return '?';
+    }
+    else if(num  == 3) {
+        return ',';
+    }
+    else if(num == 4) {
+        return '.';
+    }
+    else if(num == 5) {
+        return ' ';
+    }
+    else if(num == 6) {
+        return ';';
+    }
+    else if(num == 7) {
+        return '\"';
+    }
+    else {
+        return '\'';
+    }
+}
+
+int Decoder::modCase(int num){
+    return num % 27;
+}
+
+int Decoder::modPunctuation(int num){
+    return num % 9;
+}
+
+int Decoder::charToInt(char digit){
+    return (int) (digit - '0');
+}
+
+void Decoder::decode(int num){
+    
+    if(state == PUNCTUATION) {
+        num = modPunctuation(num);
+        if(num == 0) {
+            switchState();
+        }
+        else {
+            char character = punctuationDecode(num);
+            std::cout << character;
+        }
+    }
+    else {
+        num = modCase(num);
+        if(num == 0) {
+            switchState();
+        }
+        else if(state == UPPERCASE) {
+            char character = uppercaseDecode(num);
+            std::cout << character;
+        }
+        else if(state == LOWERCASE) {
+            char character = lowercaseDecode(num);
+            std::cout << character;
+        }
     }
 }
 //Public
@@ -48,11 +114,13 @@ void Decoder::decodeMessage() {
     int num=0;
     while(std::cin.get(charDigit)){
         int numDigit = charToInt(charDigit);
-        if(numDigit == 44){
-            //decode num;
-            //decide whether decode or switchState
-            decodeLetter(num);
+        if(charDigit == 44){
+            decode(num);
             num=0;
+        }
+        else if(charDigit == 10){
+            decode(num);
+            break;
         }
         else {
             num = (num * 10) + numDigit;
