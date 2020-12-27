416b3885ccd5b42fa44aac86703b1e41f1eae9b5
Freniel Zabala <frenielzabala@freniels-mbp.lan>
2020-12-26 16:50:57 -0800

    onto exercises

---
+++ pure_puzzles/exercises/more_shapes_to_output.cpp
@@ -0,0 +1,49 @@
+/*
+Using the same rule as the shapes programs from earlier in the chapter 
+(only two output statements—one that outputs the hash mark and one that outputs an end-of-line), 
+write a program that produces the following shapes:
+*/
+
+/*
+2-1
+########
+ ######
+  ####
+   ##
+*/
+
+/*
+2-2
+   ##
+  ####
+ ######
+########
+########
+ ######
+  ####
+   ##
+*/
+
+/*
+2-3
+#            # 
+ ##        ## 
+  ###    ### 
+   ########
+   ########
+  ###    ### 
+ ##        ## 
+#            #
+*/
+
+/*
+2-4
+########
+ ######
+  ####
+   ##
+   ##
+  ####
+ ######
+########
+*/
